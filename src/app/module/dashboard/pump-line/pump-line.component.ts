dd8c88862891c7b6aeed230d716346dcb1f0fb4b
Hasitha D <e03050@gmail.com>
2019-12-15 19:57:32 +0530

    Added water pump

--- src/app/module/dashboard/pump-line/pump-line.component.ts
+++ src/app/module/dashboard/pump-line/pump-line.component.ts
@@ -1,23 +1,132 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-pump-line',
   templateUrl: './pump-line.component.html',
   styleUrls: ['./pump-line.component.scss']
 })
-export class PumpLineComponent implements OnInit {
+export class PumpLineComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('pumpLIneContainer', { static: false }) waterContainer: ElementRef;
+  svg = document.getElementById('pump-line-canvas');
 
   pumpLineStart = 0;
   pumpLineEnd = 200;
 
-  fertInputStart = 10;
-  fertLineHeight = 20;
+  fertInputStart = 200;
+  fertLineHeight = 100;
 
-  pumping = false;
+  pumpLineWidth = 15;
+
+  pumping = true;
+  fertilizing = true;
+
+
+  viewWidth: number;
+  viewHeight: number;
+  mainPump: string;
+  fertPump: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  ngAfterViewInit(): void {
+    this.svg = document.getElementById('pump-line-canvas');
+    this.getViewDimentions();
+    this.drawPumpView();
+    this.pumpFertilizers();
+  }
+
+  getViewDimentions() {
+    this.viewWidth = this.waterContainer.nativeElement.offsetWidth;
+    this.viewHeight = this.waterContainer.nativeElement.offsetHeight;
+  }
+
+  drawPumpView() {
+    this.mainPump = `M${0},${this.pumpLineWidth / 2} ${this.viewWidth},${this.pumpLineWidth / 2}`;
+    const outputPumpLine = this.drawPumpLine(this.mainPump, this.pumpLineWidth, 'cornflowerblue');
+    this.svg.appendChild(outputPumpLine);
+
+    const outputPumpLineIn = this.drawPumpLine(this.mainPump, this.pumpLineWidth - 10, 'red');
+    this.svg.appendChild(outputPumpLineIn);
+
+    this.fertPump = `M${this.fertInputStart},${this.fertLineHeight} 
+    ${this.fertInputStart},${this.pumpLineWidth - (this.pumpLineWidth - (this.pumpLineWidth - 10)) / 2}`;
+    const fertPumpLine = this.drawPumpLine(this.fertPump, this.pumpLineWidth, 'cornflowerblue');
+    this.svg.appendChild(fertPumpLine);
+
+
+    const fertPumpLineIn = this.drawPumpLine(this.fertPump, this.pumpLineWidth - 10, 'red');
+    this.svg.appendChild(fertPumpLineIn);
+
+  }
+
+  drawPumpLine(path: string, width: number, fill: string): SVGPathElement {
+    const newElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    newElement.setAttribute('d', path);
+    newElement.style.stroke = fill;
+    newElement.style.fill = 'none';
+    newElement.style.strokeWidth = width.toString();
+    this.svg.appendChild(newElement);
+
+    return newElement;
+  }
+
+  pumpFertilizers() {
+    if (this.pumping) {
+      const pumpPath = this.drawPumpLine(this.mainPump, 0, 'none');
+      this.pumpFertilizer(1, pumpPath);
+    }
+
+    if (this.fertilizing) {
+      const fertPump = this.drawPumpLine(this.fertPump, 0, 'none');
+      this.pumpFertilizer(2, fertPump);
+    }
+
+  }
+
+  pumpFertilizer(index: number, pumpPath: any) {
+    const curveLength = pumpPath.getTotalLength();
+    const numOfPoints = curveLength / 30;
+
+    let counter = 0;
+    const dropSize = (this.pumpLineWidth - 2).toString();
+    for (let i = 0; i < numOfPoints; i++) {
+      const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+      rect.setAttribute('width', dropSize);
+      rect.setAttribute('height', dropSize);
+      rect.setAttribute('class', 'drop' + index);
+      rect.style.fill = 'yellow';
+      this.svg.appendChild(rect);
+      counter += 50;
+    }
+
+    let shift = 0;
+    const drops = document.getElementsByClassName('drop' + index);
+
+    function moveDrops() {
+
+      for (let i = 0; i < drops.length; i++) {
+        const drop = drops[i];
+        const dropWidth = Number.parseInt(drop.getAttribute('width'), 10);
+        drop.setAttribute('transform', 'translate(' + (pumpPath.getPointAtLength(i * 30 + shift).x - dropWidth / 2) + ',' +
+          (pumpPath.getPointAtLength(i * 30 + shift).y - dropWidth / 2) + ')');
+      }
+
+      shift += 10;
+      if (shift === 30) {
+        shift = 0;
+      }
+
+      setTimeout(() => {
+        requestAnimationFrame(moveDrops);
+      }, 200);
+    }
+
+    requestAnimationFrame(moveDrops);
+
+
   }
 
 }
